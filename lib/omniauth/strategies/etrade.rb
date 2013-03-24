61b6f93bf4de9f5d2de50d7a53ffaa954d9c1865
Athens Holloway <athensholloway@gmail.com>
2013-03-23 23:26:17 -0400

    changed imolementation. still learning how this works

--- lib/omniauth/strategies/etrade.rb
+++ lib/omniauth/strategies/etrade.rb
@@ -1,41 +1,39 @@
-require 'omniauth/strategies/oauth'
+require 'omniauth'
 
 module OmniAuth
   module Strategies
-    class Etrade < OmniAuth::Strategies::OAuth
+    class SomeSite < OmniAuth::Strategies::OAuth
+      
       option :name, "etrade"
+      
+      # This is where you pass the options you would pass when
+            # initializing your consumer from the OAuth gem.
+            option :client_options, {:site => "https://etws.etrade.com"}
 
-      option :client_options, {
-        :site => 'https://etws.etrade.com',
-        :request_token_path => '/oauth/request_token/',
-        :access_token_path => '/oauth/access_token/',
-        :authorize_path => '/oauth/authorize/',
-        :http_method => :get,
-        :scheme => :query_string
-      }
+            # These are called after authentication has succeeded. If
+            # possible, you should try to set the UID without making
+            # additional calls (if the user id is returned with the token
+            # or as a URI parameter). This may not be possible with all
+            # providers.
+            uid{ request.params['user_id'] }
 
-      uid do
-        user_data['profile_id']
-      end
+            info do
+              {
+                :name => raw_info['name'],
+                :location => raw_info['city']
+              }
+            end
 
-      info do
-        user_data.delete_if { |k, v| v.nil? || v.length == 0 }
-      end
+            extra do
+              {
+                'raw_info' => raw_info
+              }
+            end
 
-      extra do
-        user_data
-      end
-
-      def user_data
-        @info ||= MultiJson.decode(@access_token.get('/oapi/profiles/info/me').body)['main']
-      end
-
-      # For callback_confirmed to true so omniauth-oauth will set the
-      # :oauth_verifier during callback
-      def callback_phase
-        session['oauth'][name.to_s]['callback_confirmed'] = true
-        super
-      end
+            def raw_info
+              @raw_info ||= MultiJson.decode(access_token.get('/me.json')).body
+            end
+      
     end
   end
 end
