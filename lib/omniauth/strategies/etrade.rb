e3f6be2de80f7232b8621439b1048893ea692d4e
Athens Holloway <athensholloway@gmail.com>
2013-03-23 23:46:28 -0400

    updated implemention

--- lib/omniauth/strategies/etrade.rb
+++ lib/omniauth/strategies/etrade.rb
@@ -6,34 +6,22 @@ module OmniAuth
       
       option :name, "etrade"
       
-      # This is where you pass the options you would pass when
-            # initializing your consumer from the OAuth gem.
-            option :client_options, {:site => "https://etws.etrade.com"}
-
-            # These are called after authentication has succeeded. If
-            # possible, you should try to set the UID without making
-            # additional calls (if the user id is returned with the token
-            # or as a URI parameter). This may not be possible with all
-            # providers.
-            uid{ request.params['user_id'] }
-
-            info do
-              {
-                :name => raw_info['name'],
-                :location => raw_info['city']
-              }
-            end
-
-            extra do
-              {
-                'raw_info' => raw_info
-              }
-            end
-
-            def raw_info
-              @raw_info ||= MultiJson.decode(access_token.get('/me.json')).body
-            end
+      def initialize(app, consumer_key = nil, consumer_secret = nil, options = {}, &block)
+        opts = {
+          :site               => "https://etws.etrade.com",
+          :request_token_path => "/oauth/request_token",
+          :access_token_path  => "/oauth/access_token",
+          :authorize_url      => "https://us.etrade.com/e/t/etws/authorize"
+        }
+        super(app, :etrade, consumer_key, consumer_secret, opts, options, &block)
+      end
       
+      def auth_hash
+        OmniAuth::Utils.deep_merge(super, {
+          'extra'     => { 'user_hash' => @user_hash }
+        })
+      end
+            
     end
   end
 end
