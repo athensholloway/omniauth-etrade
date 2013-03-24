150854aa492438a8825c457165ae6fcc40a72f55
Athens Holloway <athensholloway@gmail.com>
2013-03-24 08:48:25 -0400

    updated implemention

--- lib/omniauth/strategies/etrade.rb
+++ lib/omniauth/strategies/etrade.rb
@@ -5,10 +5,10 @@ module OmniAuth
     class Etrade < OmniAuth::Strategies::OAuth
       callback_url = ''
       option :client_options, {
-        :site               => "https://etws.etrade.com",
-        :request_token_path => "/oauth/request_token",
-        :access_token_path  => "/oauth/access_token",
-        :authorize_url      => "https://us.etrade.com/e/t/etws/authorize"
+        :site => 'https://api.mendeley.com',
+        :request_token_path => '/oauth/request_token/',
+        :access_token_path => '/oauth/access_token/',
+        :authorize_path => '/oauth/authorize/',
       }
                   
     end
