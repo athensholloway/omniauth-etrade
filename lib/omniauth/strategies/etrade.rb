0960b201508fddac2e3cdcf25d531966beb50401
Athens Holloway <athensholloway@gmail.com>
2013-03-24 08:52:47 -0400

    updated implemention

--- lib/omniauth/strategies/etrade.rb
+++ lib/omniauth/strategies/etrade.rb
@@ -8,6 +8,8 @@ module OmniAuth
         :request_token_path => '/oauth/request_token/',
         :access_token_path => '/oauth/access_token/',
         :authorize_path => '/oauth/authorize/',
+        :http_method => :get,
+        :scheme => :query_string
       }
                   
     end
