9164877128be7f45e9e5da022a5c72ddef4f2fc4
Athens Holloway <athensholloway@gmail.com>
2013-03-24 00:21:52 -0400

    updated implemention

--- lib/omniauth/strategies/etrade.rb
+++ lib/omniauth/strategies/etrade.rb
@@ -3,13 +3,17 @@ require 'omniauth-oauth'
 module OmniAuth
   module Strategies
     class Etrade < OmniAuth::Strategies::OAuth
-      
+      callback_url = ''
       option :client_options, {
         :site               => "https://etws.etrade.com",
         :request_token_path => "/oauth/request_token",
         :access_token_path  => "/oauth/access_token",
         :authorize_url      => "https://us.etrade.com/e/t/etws/authorize"
       }
+      
+      def callback_url
+        "http://localhost:3000/auth/etrade/callback"
+      end
       
       uid do
         ""
