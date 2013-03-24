bb2b5ca1627358d86c84313c14de44c2ef872804
Athens Holloway <athensholloway@gmail.com>
2013-03-24 00:02:23 -0400

    updated implemention

--- lib/omniauth/strategies/etrade.rb
+++ lib/omniauth/strategies/etrade.rb
@@ -4,24 +4,27 @@ module OmniAuth
   module Strategies
     class Etrade < OmniAuth::Strategies::OAuth
       
-      option :name, "etrade"
+      option :client_options, {
+        :site               => "https://etws.etrade.com",
+        :request_token_path => "/oauth/request_token",
+        :access_token_path  => "/oauth/access_token",
+        :authorize_url      => "https://us.etrade.com/e/t/etws/authorize"
+      }
       
-      def initialize(app, consumer_key = nil, consumer_secret = nil, options = {}, &block)
-        opts = {
-          :site               => "https://etws.etrade.com",
-          :request_token_path => "/oauth/request_token",
-          :access_token_path  => "/oauth/access_token",
-          :authorize_url      => "https://us.etrade.com/e/t/etws/authorize"
-        }
-        super(app, :etrade, consumer_key, consumer_secret, opts, options, &block)
+      uid { "uid" }
+
+      info do
+        { }
+      end
+     
+      extra do
+        { }
       end
       
-      def auth_hash
-        OmniAuth::Utils.deep_merge(super, {
-          'extra'     => { 'user_hash' => @user_hash }
-        })
+      raw_info do
+        { }
       end
-            
+                  
     end
   end
 end
